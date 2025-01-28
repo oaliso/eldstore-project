8425aa105a445fc167f8d5066824edb7ed51ccbc
oaliso <alissoncsilva0511@gmail.com>
2025-01-28 15:21:13 -0400

    Created Router

--- eldstore/src/app/app.routes.ts
+++ eldstore/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { EditComponent } from './edit/edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'edit', component: EditComponent}
+];
--- eldstore/src/app/navbar/navbar.component.ts
+++ eldstore/src/app/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
