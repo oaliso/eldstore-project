bb49ed858623e72b6f259b32bf6a157595173cf1
oaliso <alissoncsilva0511@gmail.com>
2025-01-31 16:27:23 -0400

    Minimal Changes in design and all routes has created

--- eldstore/src/app/dashboard/dashboard.component.ts
+++ eldstore/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
     quantity = 1500
-}
+    adm = 351
+    ddm = 452
+    fdm = 12
+  }
