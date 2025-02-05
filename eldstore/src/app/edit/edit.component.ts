f1edfae5b0732a93c93c0919ec2f306af0157f14
Daniela Gomes <stherffanygomesz@gmail.com>
2025-02-05 13:19:07 -0400

    Barcode

--- eldstore/src/app/edit/edit.component.ts
+++ eldstore/src/app/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import JsBarcode from 'jsbarcode';
 
 @Component({
   selector: 'app-edit',
@@ -7,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './edit.component.css'
 })
 export class EditComponent {
+  generatedCode: string = '';
+  contador: number = 0;
 
+  somar() {
+    this.contador++;
+  }
+
+  subtrair() {
+    this.contador--;
+  }
+
+    @ViewChild('barcode') barcodeElement!: ElementRef;
+  
+    generateRandomCode(): string {
+      return Math.floor(100000000000 + Math.random() * 900000000000).toString();
+    }
+  
+    generateBarcode(): void {
+      this.generatedCode = this.generateRandomCode();
+  
+      JsBarcode(this.barcodeElement.nativeElement, this.generatedCode, {
+        format: 'EAN13',
+        lineColor: '#000',
+        width: 2,
+        height: 50,
+        displayValue: true
+      });
+    }
 }
--- eldstore/src/app/register/register.component.ts
+++ eldstore/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import JsBarcode from 'jsbarcode';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  generatedCode: string = '';
+  contador: number = 0;
 
+  somar() {
+    this.contador++;
+  }
+
+  subtrair() {
+    this.contador--;
+  }
+
+  @ViewChild('barcode') barcodeElement!: ElementRef;
+
+  generateRandomCode(): string {
+    return Math.floor(100000000000 + Math.random() * 900000000000).toString();
+  }
+
+  generateBarcode(): void {
+    this.generatedCode = this.generateRandomCode();
+
+    JsBarcode(this.barcodeElement.nativeElement, this.generatedCode, {
+      format: 'EAN13',
+      lineColor: '#000',
+      width: 2,
+      height: 50,
+      displayValue: true
+    });
+  }
 }
